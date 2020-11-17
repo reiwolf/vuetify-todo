40c02e51927729659835ced769f49ca15de3079b
reiwolf <reinaldo.guth@gmail.com>
2020-11-17 19:33:30 -0300

    master

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about pa-6">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit nihil voluptatem repellendus nemo eos amet eius officiis excepturi at. Repudiandae eum corrupti error nihil vel ad commodi laudantium, officiis aperiam.</p>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid dolore, necessitatibus eligendi recusandae vel cupiditate. Soluta maxime perspiciatis consequuntur adipisci placeat dignissimos quaerat necessitatibus accusantium pariatur voluptatibus modi, natus qui.</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quaerat tempora blanditiis tempore ea corrupti enim? Exercitationem velit, doloribus, facere commodi, rerum quas officiis iste omnis ab amet a quasi.</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga laborum minima veniam aliquid tempora voluptas explicabo, in officiis architecto aperiam, temporibus nihil quam. Quis quaerat, dolorum odio ea maiores dicta.</p>
+  </div>
+</template>
